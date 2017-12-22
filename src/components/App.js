fb138387fd55bca71a283b89314ddfa099416e6a
masakistewart <stewartcairo@gmail.com>
2017-12-21 20:56:06 -0500

    add front end to heroku

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ChartView from './ChartView'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.getJsonData()
+    this.state = {
+      json: this.props.json,
+      selectedJSON: this.props.json,
+    }
+  }
+
+  changeTargetNodeOnChart(json) {
+    this.setState({
+      selectedJSON: json
+    })
+  }
+
+  async getJsonData() {
+    let scrapedJson = await fetch('https://dry-sands-95822.herokuapp.com/')
+    scrapedJson = await scrapedJson.json()
+    await console.log(scrapedJson)
+    await this.changeTargetNodeOnChart(scrapedJson)
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ChartView
+          rootData={this.state.json}
+          data={this.state.selectedJSON}
+          changeTargetNodeOnChart={this.changeTargetNodeOnChart.bind(this)}
+        />
       </div>
     );
   }
